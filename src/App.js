81d1fcfabb4f2ed98fa345462e54993ade3b1804
audrbar <audrbar@gmail.com>
2023-05-01 20:29:16 +0300

    order

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Container } from '@material-ui/core';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import PostDetails from './components/PostDetails/PostDetails';
+import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
+import Auth from './components/Auth/Auth';
+import CreatorOrTag from './components/CreatorOrTag/CreatorOrTag';
+
+const App = () => (
+  <BrowserRouter>
+    <Container maxWidth="xl">
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={() => <Redirect to="/posts" />} />
+        <Route path="/posts" exact component={Home} />
+        <Route path="/posts/search" exact component={Home} />
+        <Route path="/posts/:id" exact component={PostDetails} />
+        <Route path={['/creators/:name', '/tags/:name']} component={CreatorOrTag} />
+        <Route
+          path="/auth"
+          exact
+          component={() => {
+            const user = JSON.parse(localStorage.getItem('profile'));
+            return !user ? <Auth /> : <Redirect to="/posts" />;
+          }}
+        />
+      </Switch>
+    </Container>
+  </BrowserRouter>
+);
 
 export default App;
