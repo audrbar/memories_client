8d013af7cecbaa00d25776a8983ea698349c804b
audrbar <audrbar@gmail.com>
2023-05-14 16:47:46 +0300

    onrender

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://audrbarmemories.herokuapp.com/' });
+const API = axios.create({ baseURL: 'https://memories-0742.onrender.com/' });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
